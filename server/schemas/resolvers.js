497889d587048905173ccbfe99a2abccdcf1f685
dejagraver <dejaamaartin@gmail.com>
2021-07-24 19:01:44 -0400

    updating code, removing pseudo. CRUD

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc'); //replace with process.env.STRIPE_KEY
 
 const resolvers = {
   Query: {
@@ -52,10 +52,11 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
+
     checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
+      const order = new Order ({products: args.products});
       const { products } = await order.populate('products').execPopulate();
+      const url = new URL(context.headers.referer).origin;
 
       const line_items = [];
 
@@ -71,14 +72,14 @@ const resolvers = {
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
-          currency: 'usd'
+          currency: 'usd',
         });
 
         // add price id to the line items array
         line_items.push({
           price: price.id,
           quantity: 1
-        })
+        });
       }
 
       const session = await stripe.checkout.sessions.create({
@@ -86,11 +87,15 @@ const resolvers = {
         line_items,
         mode: 'payment',
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}`
-      });
+        cancel_url: `${url}/`
+      })
 
-      return { session: session.id };
+      return { session: session.id}
+
     }
+
+
+
   },
   Mutation: {
     addUser: async (parent, args) => {
@@ -100,6 +105,7 @@ const resolvers = {
       return { token, user };
     },
     addOrder: async (parent, { products }, context) => {
+      console.log(context);
       if (context.user) {
         const order = new Order({ products });
 
