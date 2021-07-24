dec4e35dfce7e487a4a19db28557919243c65a25
dejagraver <dejaamaartin@gmail.com>
2021-07-24 18:56:27 -0400

    run build

---
+++ client/build/precache-manifest.1e068de72f995e8ceb7f20d123bf6f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2e8081da7b2f57bab52b629fb8a36d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47f8e90a8610abf88cef",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "c98db35b446189178b62",
+    "url": "/static/js/2.63d1a33a.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.63d1a33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f8e90a8610abf88cef",
+    "url": "/static/js/main.db985441.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
